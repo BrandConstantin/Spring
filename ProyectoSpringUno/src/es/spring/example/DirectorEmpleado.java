3cd8064acfed03d452152b3c5a4e5842ed2d675b
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-01-16 23:08:45 +0100

    Inyección de dependencias
    Inyección de dependencias mediante un constructor

--- ProyectoSpringUno/src/es/spring/example/DirectorEmpleado.java
+++ ProyectoSpringUno/src/es/spring/example/DirectorEmpleado.java
@@ -1,11 +1,24 @@
 package es.spring.example;
 
 public class DirectorEmpleado implements Empleados {
+	//creación de campo tipo creacionInforme (intefaz)
+	private CreacionInformes informeNuevo;
+	
+	//creación de constructor que inyecta la dependencia
+	public DirectorEmpleado(CreacionInformes informenuevo) {
+		this.informeNuevo = informenuevo;
+	}
 
 	@Override
 	public String getTareas() {
 		// TODO Auto-generated method stub
-		return "Gestiono toda la empresa";
+		return "Gestiono la plantilla de la empresa";
+	}
+
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe creado por el Director " + informeNuevo.getInforme();
 	}
 
 }
--- ProyectoSpringUno/src/es/spring/example/JefeEmpleado.java
+++ ProyectoSpringUno/src/es/spring/example/JefeEmpleado.java
@@ -1,9 +1,24 @@
 package es.spring.example;
 
 public class JefeEmpleado implements Empleados {
-		
+	public JefeEmpleado(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+
+	//creación de campo tipo creacionInforme (intefaz)
+	private CreacionInformes informeNuevo;
+	
+	//creación de constructor que inyecta la dependencia
+	
+	
 	public String getTareas() {
 		return "Gestiono las cuestiones relativas a mis empleados de sección";
+	}
+
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe presentado por el jefe con rectificaciones: " + informeNuevo.getInforme();
 	}
 
 }
