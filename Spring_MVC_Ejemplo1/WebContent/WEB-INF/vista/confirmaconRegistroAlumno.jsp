96f7b4001d30b3eb87325f78a43c22729f83d3ef
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-01-29 18:05:43 +0100

    Form MVC tags
    Form MVC tags

---
+++ Spring_MVC_Ejemplo1/WebContent/WEB-INF/vista/confirmaconRegistroAlumno.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Confirmación Registro</title>
+</head>
+<body>
+	<!-- utilizar las jsp tagas que viajan en el modelo -->
+	<p>El alumno con nombre ${elAlumno.nombre} y apellidos ${elAlumno.apellido} se ha registrado con exito!</p>
+</body>
+</html>
---
+++ Spring_MVC_Ejemplo1/src/es/spring/mvc/example/AlumnoController.java
@@ -0,0 +1,23 @@
+package es.spring.mvc.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/alumno")
+public class AlumnoController {
+	@RequestMapping("/muestraFormulario")
+	public String muestraFormulario(Model modelo) {
+		Alumno elAlumno = new Alumno();
+		modelo.addAttribute("elAlumno", elAlumno);
+		
+		return "alumnoRegistroFormulario";		
+	}
+	
+	@RequestMapping("/procesarFormulario")
+	public String procesarFormulario(@ModelAttribute("elAlumno") Alumno objetoAlumnoRecibido) {
+		return "confirmaconRegistroAlumno";
+	}
+}
